b130fc360473ce53c06bc168708b3862ac859d63
Laura Violla <lauraviolla.tds1.senai@gmail.com>
2025-11-27 16:19:43 -0300

    feat: create createPlaylist

--- App.js
+++ App.js
@@ -6,17 +6,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import CreatePlaylist from './pages/CreatePlaylist';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="SignUp" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="SignIn" component={SignIn} />
-      </Stack.Navigator>
-      <StatusBar style="auto" />
+      <CreatePlaylist />
     </NavigationContainer>
   );
 }
